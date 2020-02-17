18bde80ed0e8c96df80b8a3581e8113a58fec860
Jaeik Oh <flyerjayden@gmail.com>
2020-02-17 16:08:32 -0600

    Collider now will listen to port 8080

--- collider/main.go
+++ collider/main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/200106-uta-go/JKJP2/collider/proc"
 )
@@ -12,12 +11,12 @@ import (
 func main() {
 
 	// Get service port value from environment variable.
-	port := os.Getenv("SERV_PORT")
+	//port := os.Getenv("SERV_PORT")
 
 	// Response function initiator.
 	http.HandleFunc("/", proc.Resp)
 
 	// Listen http via service port as definded in enviroment variable.
-	fmt.Printf("Server is running on port %s\n", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+	fmt.Printf("Server is running on port %s\n", "8080")
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", "8080"), nil))
 }
